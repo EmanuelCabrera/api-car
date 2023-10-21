8130323361b6526664a9ffc9c1957d0d17275178
Emanuel Cabrera <emanuelc@versiondos.com>
2023-10-21 19:37:09 -0300

    add carService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { BrandModule } from './brand/brand.module';
 import { JwtModule } from './jwt/jwt.module';
+import { CarModule } from './car/car.module';
+import { JwtMiddleware } from './jwt/jwt.middleware';
+import { JwtService } from './jwt/jwt.service';
 
 @Module({
-  imports: [UserModule, BrandModule, JwtModule],
+  imports: [UserModule, BrandModule, JwtModule, CarModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(JwtMiddleware)
+      .forRoutes('brand','car');
+  }
+}
--- src/brand/brand.module.ts
+++ src/brand/brand.module.ts
@@ -1,19 +1,14 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BrandService } from './brand.service';
 import { BrandController } from './brand.controller';
 import { PrismaService } from '../prisma/prisma.service';
-import { JwtMiddleware } from 'src/jwt/jwt.middleware';
-import { JwtService } from 'src/jwt/jwt.service';
+
 
 
 @Module({
   controllers: [BrandController],
-  providers: [BrandService, PrismaService, JwtService]
+  providers: [BrandService, PrismaService]
 })
 export class BrandModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(JwtMiddleware)
-      .forRoutes('brand');
-  }
+  
 }
